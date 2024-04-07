91351353fd4cf7751097cc210c3303ad129e5f4a
Madhav kumar <madhavkumar332003@gmail.com>
2024-04-07 11:15:21 +0530

    first commit

---
+++ constants.py
@@ -0,0 +1,21 @@
+SIZE=400
+GRID_LEN=4
+GRID_PADDING=10
+
+BACKGROUND_COLOR_GAME="#92877d"
+BACKGROUND_COLOR_CELL_EMPTY="#9e948a"
+
+BACKGROUND_COLOR_DICT={2:"#eee4da",4:"#ede0c8",8:"#f2b179",16:"#f59563",32:"#f67c5f",64:"#f65e3b",128:"#edcf72",256:"#edcc61",512:"#edc850",1024:"#edc53f",2048:"#edc22e",
+                       
+4096:"#3c3a32",8192:"#3c3a32",16384:"#3c3a32",32768:"#3c3a32",65536:"#3c3a32"}
+
+CELL_COLOR_DICT={2:"#776e65",4:"#776e65",8:"#f9f6f2",16:"#f9f6f2",32:"#f9f6f2",64:"#f9f6f2",128:"#f9f6f2",256:"#f9f6f2",512:"#f9f6f2",1024:"#f9f6f2",2048:"#f9f6f2",
+                 
+4096:"#f9f6f2",8192:"#f9f6f2",16384:"#f9f6f2",32768:"#f9f6f2",65536:"#f9f6f2"}
+
+FONT= ("Verdana",40,"bold")
+
+KEY_UP="'w'"
+KEY_DOWN="'s'"
+KEY_LEFT="'a'"
+KEY_RIGHT="'d'"
